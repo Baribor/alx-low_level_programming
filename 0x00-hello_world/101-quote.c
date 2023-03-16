7ec9788b5333cf560dc58c04d73279dc4a1a710d
Baribor Saturday <saturdaybaribor@gmail.com>
2023-03-16 15:23:18 +0100

    Added advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - The main entry point
+ *
+ * Description: prints to std output without using
+ * any of the printf or puts familiy
+ *
+ * Return:Always 1 (Error)
+ */
+int main(void)
+{
+const char msg[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, sizeof(msg) - 1);
+
+return (1);
+}
