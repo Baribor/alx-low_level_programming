cba3078f5912e766b60b650348604f9c580c09f3
Baribor Saturday <saturdaybaribor@gmail.com>
2023-03-16 14:30:45 +0100

    made some corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - The entry point
+ *
+ * Description: Prints to std output using put.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - The entry point
+ *
+ * Description: Prints to std output using printf
+ * and ending with a newline char.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
