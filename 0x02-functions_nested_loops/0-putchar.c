13fa562bb25ce40e0612fe19fa94ce46406024d3
Baribor Saturday <saturdaybaribor@gmail.com>
2023-03-21 14:54:34 +0100

    Added new projects

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+
+	for (int i = 0; i < 8; ++i)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+This project aims to cover the following scope in c:
+- Nested loops
+- Functions
+- Prototype
+- Scope of variables 
+- Header files
