88f21f66544c446d3f5b549cea20d39f5c3930ad
Baribor Saturday <saturdaybaribor@gmail.com>
2023-03-22 23:18:09 +0100

    last task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the first 98 fibonacci numbers
  *
  * Return: 0 (Success)
  */
- int main(void)
- {
- 
+int main(void)
+{
+	unsigned long a = 1, b = 2, c;
+	int count = 2;
+
+	printf("%lu, %lu, ", a, b);
+
+	while (count < 97)
+	{
+		c = a + b;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		count++;
+	}
+
+	c = a + b;
+	printf("%lu\n");
+
+	return (0);
 }
