4e2732117a63841fcb8e27d875c73557277060e4
Baribor Saturday <saturdaybaribor@gmail.com>
2023-04-11 15:48:43 +0100

    changes to 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,10 +14,15 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	grid = (int **) malloc(height * sizeof(int *));
+	if (!grid)
+		return (NULL);
 
 	for (i = 0; i < height; ++i)
 	{
 		grid[i] = malloc(width * sizeof(int));
+		if (!grid[i])
+			return (NULL);
+
 		for (j = 0; j < width; ++j)
 		{
 			grid[i][j] = 0;
