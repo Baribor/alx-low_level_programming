bf044bb3bc78964cee51f13b8ed2ae276fffe392
Baribor Saturday <saturdaybaribor@gmail.com>
2023-04-11 20:34:20 +0100

    advance

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - merge all arguments as a single string
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: The merge arguments as string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s, *a;
+	int size = ac, i, j = 0, k = 0;
+
+	if (ac == 0 || !av)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i)
+		size += strlen(av[i]);
+
+	s = (char *) malloc(size * sizeof(char));
+	if (!s)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i, j = 0)
+	{
+		a = av[i];
+		while (a[j] != '\0')
+			s[k++] = a[j++];
+		s[k++] = '\n';
+	}
+	return (s);
+}
