de6562bb3fe52c7ed83ed424581c7a2205bd8a47
Baribor Saturday <saturdaybaribor@gmail.com>
2023-06-19 16:46:33 +0100

    Solved atsk 0: Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a new dog datatype
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
