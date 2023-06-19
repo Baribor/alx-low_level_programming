70040ddcd83f6f5dc491eba6d61eafce9c4810c8
Baribor Saturday <saturdaybaribor@gmail.com>
2023-06-19 16:47:52 +0100

    Solved task 1: A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type dog
+ * @d: the dog pointer
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
