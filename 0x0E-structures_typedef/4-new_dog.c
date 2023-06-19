596c8b2bd7001d6987539e7f98aba752562456c0
Baribor Saturday <saturdaybaribor@gmail.com>
2023-06-19 18:12:31 +0100

    Fixed bug in task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,40 +11,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+
 	dog_t *d;
 	int sn, so;
 	char *nsn;
 	char *nso;
 
-	if (!name)
+	if (!(name && owner))
+		return (NULL);
+
+	sn = strlen(name);
+	so = strlen(owner);
+
+	nsn = (char *)malloc((sn * sizeof(char)) + 1);
+
+	if (!nsn)
+		return (NULL);
+
+	nso = (char *)malloc((so * sizeof(char)) + 1);
+	if (!nso)
 	{
-		nsn = NULL;
-	}
-	else
-	{
-		sn = strlen(name);
-		nsn = (char *)malloc((sn * sizeof(char)) + 1);
-		if (!nsn)
-			return (NULL);
-		nsn = strcpy(nsn, name);
+		free(nsn);
+		return (NULL);
 	}
 
-	if (!owner)
-	{
-		nso = NULL;
-	}
-	else
-	{
-		so = strlen(owner);
-		nso = (char *)malloc((so * sizeof(char)) + 1);
-		if (!nso)
-			return (NULL);
-		nso = strcpy(nso, name);
-	}
+	nsn = strcpy(nsn, name);
+	nso = strcpy(nso, owner);
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
+	{
+		free(nsn);
+		free(nso);
 		return (NULL);
+	}
+
 	d->name = nsn;
 	d->age = age;
 	d->owner = nso;
