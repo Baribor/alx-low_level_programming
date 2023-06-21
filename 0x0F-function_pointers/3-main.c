b83058bc2d8dae5a798ddaa1f462561c1932af5a
Baribor Saturday <saturdaybaribor@gmail.com>
2023-06-21 17:08:59 +0100

    Fixed some bugs in task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,16 +2,40 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - program that performs simple operations
+ * @argc: argument counter
+ * @argv: Argument values
+ * Return: 0 always
+ */
 int main(int argc, char *argv[])
 {
-    int (*f)(int, int);
-    int result;
+	int (*f)(int, int);
+	int result, left, right;
 
-    if (argc != 4)
-        return (-1);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    f = get_op_func(argv[2]);
-    result = f(atoi(argv[1]), atoi(argv[3]));
-    printf("%d\n", result);
-    return (0);
+	left = atoi(argv[1]);
+	right = atoi(argv[3]);
+
+	if (left == 0 && (*(argv[2]) == '/' || *(argv[2]) == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	f = get_op_func(argv[2]);
+
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = f(left, right);
+	printf("%d\n", result);
+	return (0);
 }
