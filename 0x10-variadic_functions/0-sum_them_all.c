20109c9acafe3160c9cb1c1f750d68fdec6ec5ea
Baribor Saturday <saturdaybaribor@gmail.com>
2023-06-22 16:48:45 +0100

    Solved task 0: Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,20 +7,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum = 0;
-    int i;
-    va_list args;
+	int sum = 0;
+	unsigned int i;
+	va_list args;
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; ++i)
-    {
-        sum += va_arg(args, int);
-    }
-    va_end(args);
+	for (i = 0; i < n; ++i)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
 
-    return (sum);
+	return (sum);
 }
