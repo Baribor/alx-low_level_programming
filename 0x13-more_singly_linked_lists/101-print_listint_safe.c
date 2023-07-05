2a5138ab1b995f288c60c31a0b7e905cbd9e822f
Baribor Saturday <saturdaybaribor@gmail.com>
2023-07-05 10:12:15 +0100

    Solve task 12: Print (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a list with a loop
+ * @head: Head of the list
+ * Return: The length of the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+    size_t len = 0;
+    const listint_t *currentNode = head;
+
+    while (currentNode)
+    {
+        len++;
+        printf("[%p] %d\n", (void *)currentNode, currentNode->n);
+        if (currentNode->next > currentNode)
+            return (len);
+        currentNode = currentNode->next;
+    }
+
+    return (len);
+}
