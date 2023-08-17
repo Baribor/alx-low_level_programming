e403fb5072b1bce05ea9b2eb41815098471002ac
Baribor Saturday <saturdaybaribor@gmail.com>
2023-08-17 10:57:15 +0100

    solved task 3: Add node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Add a node to the end of the list
+ * @head: The head of the list
+ * @n: The new node
+ * Return: Pointer to the new node else NULL on failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current_head;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t *));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	current_head = *head;
+	if (!current_head)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		while (current_head->next)
+			current_head = current_head->next;
+		new_node->prev = current_head;
+		current_head->next = new_node;
+	}
+
+	return (new_node);
+}
