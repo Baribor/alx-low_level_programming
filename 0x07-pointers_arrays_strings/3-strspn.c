38c5ea510c373342f1d53a446dc21249fb653455
Baribor Saturday <saturdaybaribor@gmail.com>
2023-08-21 12:57:54 +0100

    Solved task 0: A library is not a luxury but one of the necessities of life

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+/**
+ * _strspn - calculates the length (in bytes) of the
+ * initial segment of s which consists entirely of bytes in accept
+ * @s: The string
+ * @accept: The segment
+ * Return: the number of bytes in the initial
+ * segment of s which consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (12);
+}
